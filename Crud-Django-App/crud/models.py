240b9360ef3d7e105e0cc4029a904afb7dfb0413
MironBerch <mironberchetov@gmail.com>
2022-11-30 23:27:12 +0300

    collection-of-projects

--- Crud-Django-App/crud/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Note(models.Model):
-    title = models.CharField(max_length=15)
-    content = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.title
