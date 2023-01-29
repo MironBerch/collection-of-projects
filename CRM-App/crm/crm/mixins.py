c50e395c2c60333aca9192de85d8fb9aab017b57
MironBerch <mironberchetov@gmail.com>
2023-01-29 21:36:27 +0300

    CRM-App

--- CRM-App/crm/crm/mixins.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.mixins import AccessMixin
-from django.shortcuts import redirect
-
-
-class OrganisorAndLoginRequiredMixin(AccessMixin):
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated or not request.user.is_organisor:
-            return redirect("leads:lead-list")
-        return super().dispatch(request, *args, **kwargs)
