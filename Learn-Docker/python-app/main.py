4559289dc78cbc3a703a862bf6f1688aef19125e
MironBerch <mironberchetov@gmail.com>
2023-03-27 21:57:44 +0300

    Learn-Docker

---
+++ Learn-Docker/python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Добро пожаловать в супер календарь')
+
+year = int(input())
+month = int(input())
+
+print(calendar.month(year, month))
+
+print('Всего хорошего!')
---
+++ Learn-Docker/python-app/manage.py
@@ -0,0 +1,5 @@
+def hello_docker():
+    return 'Hello Docker'
+
+message = hello_docker()
+print(message)
