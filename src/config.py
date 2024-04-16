00e6b1d485807e1a167d44d4ea7dfe5875dbb503
MironBerch <mironberchetov@gmail.com>
2024-04-16 18:56:04 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -7,3 +7,5 @@ TELEGRAM_API_TOKEN: str | None = environ.get('TELEGRAM_API_TOKEN')
 BASE_DIR = Path(__file__).resolve().parent
 
 TEMPLATES_DIR = BASE_DIR / 'templates'
+
+IO_NET_API_KEY: str | None = environ.get('IO_NET_API_KEY')
