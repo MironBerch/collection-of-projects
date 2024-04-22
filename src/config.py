08fea64db4dd445cc98088aa4077d9ee65fd6c52
MironBerch <mironberchetov@gmail.com>
2024-04-22 16:19:06 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
 from os import environ
+from pathlib import Path
 
 
 TELEGRAM_API_TOKEN: str | None = environ.get('TELEGRAM_API_TOKEN')
+
+BASE_DIR = Path(__file__).resolve().parent
+
+TEMPLATES_DIR = BASE_DIR / 'templates'
