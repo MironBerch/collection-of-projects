0f06d4f9429235188554eb4d5b81f127fcb3d76f
MironBerch <mironberchetov@gmail.com>
2024-11-09 00:29:32 +0300

    Update logger

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,11 +1,18 @@
+import os
 import logging
 from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger('__main__')
 logger.setLevel(logging.INFO)
 
+try:
+    os.mkdir('../logs')
+except FileExistsError:
+    pass
+
+
 file_handler = RotatingFileHandler(
-    'main.log',
+    '../logs/main.log',
     maxBytes=48*1024*1024,
     backupCount=5,
 )
