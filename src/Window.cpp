06f90cbaa6526ac15e90ccfa02b02d7d72741324
MironBerch <mironberchetov@gmail.com>
2025-06-17 02:37:09 +0300

    Edit Window.cpp imports

--- src/Window.cpp
+++ src/Window.cpp
@@ -6,10 +6,8 @@
 #include "AUI/ASS/Property/Border.h"
 #include "AUI/ASS/Property/TransformOffset.h"
 #include "AUI/ASS/Selector/on_state.h"
-#include "Window.h"
 #include "AUI/Layout/AVerticalLayout.h"
 #include "AUI/Model/ATreeModel.h"
-#include "AUI/Util/AMetric.h"
 #include "AUI/View/A2FingerTransformArea.h"
 #include "AUI/View/AButton.h"
 #include "AUI/Layout/AHorizontalLayout.h"
@@ -17,37 +15,25 @@
 #include "AUI/View/ACircleProgressBar.h"
 #include "AUI/View/ALabel.h"
 #include "AUI/Layout/AStackedLayout.h"
-#include "AUI/View/ACheckBox.h"
-#include "AUI/View/AProgressBar.h"
 #include "AUI/View/ATextField.h"
-#include "AUI/View/ANumberPicker.h"
-#include "AUI/View/ADoubleNumberPicker.h"
 #include "AUI/View/ASpacerExpanding.h"
 #include "AUI/Util/UIBuildingHelpers.h"
 #include "AUI/View/ASpinnerV2.h"
 #include "AUI/View/AGroupBox.h"
-#include "AUI/View/ADragNDropView.h"
 #include "AUI/Util/ALayoutInflater.h"
 #include "AUI/View/ASlider.h"
 #include "AUI/Platform/APlatform.h"
 #include "AUI/IO/AByteBufferInputStream.h"
 #include "AUI/View/ASpinnerV2.h"
-#include <AUI/Model/AListModel.h>
-#include <AUI/View/ADropdownList.h>
 #include <AUI/i18n/AI18n.h>
 #include <AUI/i18n/AI18n.h>
 #include <AUI/ASS/ASS.h>
-#include <AUI/View/AListView.h>
 #include <AUI/View/ATextArea.h>
 #include <AUI/View/ARulerView.h>
 #include <AUI/View/AForEachUI.h>
 #include <AUI/View/ARulerArea.h>
-#include <AUI/View/ATreeView.h>
 #include <AUI/Platform/ADesktop.h>
 #include <AUI/Platform/AMessageBox.h>
-#include <AUI/View/ADragArea.h>
-#include <memory>
-#include <random>
 #include <AUI/View/ASplitter.h>
 #include <AUI/View/AScrollArea.h>
 #include <AUI/View/ATabView.h>
@@ -55,6 +41,11 @@
 #include <AUI/View/AText.h>
 #include <AUI/View/ADrawableView.h>
 #include <AUI/Traits/platform.h>
+
+#include <memory>
+#include <random>
+
+#include "Window.h"
 
 using namespace declarative;
 
